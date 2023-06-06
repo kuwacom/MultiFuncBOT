004d12a91e01ae723aa3521e89d6e136951288eb
kuwacom <kuwa.com3@gmail.com>
2023-06-06 21:48:02 +0900

    online-link

--- src/commands/poll.ts
+++ src/commands/poll.ts
@@ -6,28 +6,35 @@ import { interaction } from "../format/error";
 
 export const command = {
     name: "poll",
-    description: "アンケートを開始します",
-    type: 1,
+    description: "アンケートコマンド",
     options: [
         {
-            name: "switch",
-            description: "複数投票を許可するか",
-            required: true,
-            type: 5
-        },
-        {
-            name: "title",
-            description: "アンケートのタイトル",
-            required: true,
-            type: 3
-        },
-        {
-            name: "description",
-            description: "アンケートの詳細",
-            required: false,
-            type: 3
+            name: "create",
+            description: "アンケートを作成します",
+            type: 1
         }
     ]
+    // type: 1,
+    // options: [
+    //     {
+    //         name: "switch",
+    //         description: "複数投票を許可するか",
+    //         required: true,
+    //         type: 5
+    //     },
+    //     {
+    //         name: "title",
+    //         description: "アンケートのタイトル",
+    //         required: true,
+    //         type: 3
+    //     },
+    //     {
+    //         name: "description",
+    //         description: "アンケートの詳細",
+    //         required: false,
+    //         type: 3
+    //     }
+    // ]
 }
 
 
@@ -40,23 +47,51 @@ export const executeInteraction = async (interaction: Types.DiscordCommandIntera
     if (!interaction.guild || !interaction.channel || !interaction.member || !interaction.isChatInputCommand()) return;
     // interactionCommand
 
-    const switchBoolean = interaction.options.getBoolean("swich");
-    const title = interaction.options.getString("title");
-    const description = interaction.options.getString("description", false);
+    // const switchBoolean = interaction.options.getBoolean("swich");
+    // const title = interaction.options.getString("title");
+    // const description = interaction.options.getString("description", false);
 
-    interaction.reply({
-        embeds: [
-            new Discord.EmbedBuilder()
-            .setColor(Types.embedCollar.success)
-            .setTitle(title)
-            .setDescription(description)
-            .setFooter({ iconURL: interaction.user.avatarURL() as string, text: `${interaction.user.username}#${interaction.user.discriminator}\n` +
-                config.embed.footerText
-            })
-        ],
-        allowedMentions: {
-            repliedUser: false
-        },
-        ephemeral: true
-    });
+    // interaction.reply({
+    //     embeds: [
+    //         new Discord.EmbedBuilder()
+    //         .setColor(Types.embedCollar.success)
+    //         .setTitle(title)
+    //         .setDescription(description)
+    //         .setFooter({ iconURL: interaction.user.avatarURL() as string, text: `${interaction.user.username}#${interaction.user.discriminator}\n` +
+    //             config.embed.footerText
+    //         })
+    //     ],
+    //     allowedMentions: {
+    //         repliedUser: false
+    //     },
+    //     ephemeral: true
+    // });
+
+
+    const modal = new Discord.ModalBuilder()
+        .setCustomId('create')
+        .setTitle('ポールの作成');
+
+    modal.addComponents(
+        new Discord.ActionRowBuilder<Discord.TextInputBuilder>().addComponents(
+            new Discord.TextInputBuilder()
+                .setCustomId('title')
+                .setLabel('ポールのタイトル')
+                .setStyle(Discord.TextInputStyle.Short)
+        ),
+        new Discord.ActionRowBuilder<Discord.TextInputBuilder>().addComponents(
+            new Discord.TextInputBuilder()
+                .setCustomId('description')
+                .setLabel('ポールの詳細')
+                .setStyle(Discord.TextInputStyle.Paragraph)
+        ),
+        new Discord.ActionRowBuilder<Discord.TextInputBuilder>().addComponents(
+            new Discord.TextInputBuilder()
+                .setCustomId('contents')
+                .setLabel('アンケート項目 ( : で項目を区切ってください)')
+                .setStyle(Discord.TextInputStyle.Paragraph)
+        )
+        );
+
+    await interaction.showModal(modal);
 }
